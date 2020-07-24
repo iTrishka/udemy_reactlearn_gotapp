db63edb285c64e581359c5204bc21e3eb8bf9636
Trishka <snowflak@yandex.ru>
2020-07-24 11:51:35 +0300

    audit fix

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -32,8 +32,8 @@ export default class RandomChar extends Component {
     }
 
     updateChar(){
-        const id = Math.floor(Math.random()*140+25); //25-140
-        // const id = 1300000;
+        // const id = Math.floor(Math.random()*140+25); //25-140
+        const id = 1300000;
         this.gotService.getCharacter(id)
             .then(this.onCharLoaded)
             .catch(this.onError)
